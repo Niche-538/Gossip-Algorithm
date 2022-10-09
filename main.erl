f9d831dbd09bff71e0846c29a59122c4adb400c3
Anurag Patil <41022671+patilanurag@users.noreply.github.com>
2022-10-09 12:47:21 -0400

    List of Actors

--- main.erl
+++ main.erl
@@ -1,18 +1,33 @@
 -module(main).
--export([main_process/3]).
+-export([main_process/3, start/1]).
+-import(lists, [append/2, reverse/1]).
 -import(gossip, [gossip/2]).
 -import(push_sum, [push_sum/2]).
 
-
-
 main_process(NumNodes, Topology, Algorithm) ->
-
-  if Algorithm == "Gossip" ->
-    gossip(NumNodes,Topology);
-  true ->
-    push_sum(NumNodes,Topology)
+    if
+        Algorithm == "Gossip" ->
+            gossip(NumNodes, Topology);
+        true ->
+            push_sum(NumNodes, Topology)
     end.
 
+start(Num) ->
+    L = while(Num),
+    io:fwrite("List of Actors: ~p~n", [L]),
+    io:fwrite("Len of List: ~p~n", [tail_len(L)]).
 
+while(N) ->
+    while(0, N, []).
 
+while(N, N, L) ->
+    reverse(L);
+while(S, N, L) ->
+    while(S + 1, N, [spawn(fun() -> actor_call() end) | L]).
 
+actor_call() ->
+    io:fwrite("").
+
+tail_len(L) -> tail_len(L, 0).
+tail_len([], Acc) -> Acc;
+tail_len([_ | T], Acc) -> tail_len(T, Acc + 1).
